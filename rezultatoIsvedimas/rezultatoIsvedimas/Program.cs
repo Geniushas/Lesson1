08560d5bf8a30dd6a5cf2f463e74c8f70682bd75
Geniushas <andruis0506@gmail.com>
2020-03-31 21:57:47 +0300

    rezultatoIsvedimas

---
+++ rezultatoIsvedimas/rezultatoIsvedimas/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rezultatoIsvedimas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(-6 + 3 * 5);
+            Console.WriteLine((13 - 2) * 7);
+            Console.WriteLine((5 + -2) + (20 / 10));
+
+            Console.ReadLine();
+        }
+    }
+}
