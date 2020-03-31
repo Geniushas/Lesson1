6dea291f91c770a84a9a8cc79cf4717969d7fb6a
Geniushas <andruis0506@gmail.com>
2020-03-31 21:48:15 +0300

    skaiciuDalyba

---
+++ skaiciuDalyba/skaiciuDalyba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skaiciuDalyba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double skaiciusVienas = 24;
+            double skaiciusDu = 5;
+
+            Console.WriteLine(skaiciusVienas / skaiciusDu);
+            Console.ReadLine();
+        }
+    }
+}
