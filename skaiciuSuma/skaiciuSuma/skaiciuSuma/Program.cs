366c073be34771a2779eb6d780c0f12415b58152
Geniushas <andruis0506@gmail.com>
2020-03-31 21:41:28 +0300

    skaiciuSuma

---
+++ skaiciuSuma/skaiciuSuma/skaiciuSuma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skaiciuSuma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pirmasSkaicius = 17;
+            int antrasSkaicius = 23;
+
+            Console.WriteLine(pirmasSkaicius + antrasSkaicius);
+            Console.ReadLine();
+        }
+    }
+}
