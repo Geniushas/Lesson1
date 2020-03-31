13ec2a35dd35e2031b02f4b8cf80b1765a96a0ed
Geniushas <andruis0506@gmail.com>
2020-03-31 22:57:44 +0300

    Kol kas dar ieskau info, bet ryt issiaiskinsiu

---
+++ vartotojuIvestines/vartotojuIvestines/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vartotojuIvestines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vartotojasVienas;
+            double vartotojasDu;
+
+            Console.WriteLine("Pirmasis vartotojau, iveskite savo skaiciu:");
+            Console.ReadKey();
+            Console.WriteLine("Antrasis vartotojau, iveskite savo skaiciu:");
+            Console.ReadLine();
+
+        }
+    }
+}
