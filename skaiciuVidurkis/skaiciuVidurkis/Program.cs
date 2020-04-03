a9798aa98a7b45549ee844386e0f32b8c05b1a12
Geniushas <andruis0506@gmail.com>
2020-04-03 23:51:58 +0300

    skaiciuVidurkis

---
+++ skaiciuVidurkis/skaiciuVidurkis/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace skaiciuVidurkis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zemiau iveskite keturis pasirinktus skaicius:");
+
+            Console.Write("1: ");
+            double skaiciusVienas = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("2: ");
+            double skaiciusDu = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("3: ");
+            double skaiciusTrys = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("4: ");
+            double skaiciusKeturi = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Siu skaiciu vidurkis: " + (skaiciusVienas + skaiciusDu + skaiciusTrys + skaiciusKeturi) / 4);
+
+            Console.ReadLine();
+            
+        }
+    }
+}
