b550449e17533c744a912b9a61a0f82737086eba
Geniushas <andruis0506@gmail.com>
2020-04-04 00:20:44 +0300

    staciakampisInterpolation

---
+++ staciakampisInterpolation/staciakampisInterpolation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace staciakampisInterpolation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Iveskite simboli, is kurio bus nupiestas staciakampis: ");
+            string simbolis = Console.ReadLine();
+
+            Console.WriteLine($"{simbolis}{simbolis}{simbolis}");
+            Console.WriteLine($"{simbolis} {simbolis}");
+            Console.WriteLine($"{simbolis} {simbolis}");
+            Console.WriteLine($"{simbolis} {simbolis}");
+            Console.WriteLine($"{simbolis}{simbolis}{simbolis}");
+
+            Console.ReadLine();
+        }
+    }
+}
