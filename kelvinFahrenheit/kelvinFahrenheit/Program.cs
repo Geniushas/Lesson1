f756484bc68da3e5ef82e0867db0b58d7ae87a72
Geniushas <andruis0506@gmail.com>
2020-04-04 00:32:16 +0300

    kelvinFahrenheit

---
+++ kelvinFahrenheit/kelvinFahrenheit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kelvinFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite laipsnius Celsijais: ");
+            int celsius = Convert.ToInt32(Console.ReadLine());
+
+            int kelvis = celsius + 273;
+            int farenheit = celsius * 18 / 10 + 32;
+
+            Console.WriteLine("kelvin = " + kelvis);
+            Console.WriteLine("fahrenheit = " + farenheit);
+
+            Console.ReadLine();
+        }
+    }
+}
