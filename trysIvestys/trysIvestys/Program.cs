496ac8a6ba2cfeb5421814cf85f404887de43fb5
Geniushas <andruis0506@gmail.com>
2020-04-04 00:09:55 +0300

    trysIvestys

---
+++ trysIvestys/trysIvestys/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trysIvestys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite tris pasirinktus skaicius:");
+
+            Console.Write("Pirmas skaicius: ");
+            double skaiciusA = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Antras skaicius: ");
+            double skaiciusB = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Trecias skaicius: ");
+            double skaiciusC = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("(" + skaiciusA + " + " + skaiciusB + ") * " + skaiciusC + " = " + (skaiciusA + skaiciusB) * skaiciusC);
+            Console.WriteLine(skaiciusA + " * " + skaiciusC + " + " + skaiciusB + " * " + skaiciusC + " = " + (skaiciusA * skaiciusC + skaiciusB * skaiciusC));
+
+            Console.ReadLine();
+        }
+    }
+}
